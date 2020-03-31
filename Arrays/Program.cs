d9a1153eb92de9c88bae1883aca06db684c9f189
Gomes <gionni.lucio.gomes@accenture.com>
2020-03-30 21:54:35 -0300

    Vetores C

---
+++ Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numero = { 1, 2, 3, 4, 5 };
+
+            int[] arrayInt = new int[10];
+
+           
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                arrayInt[i] = i + 1;
+                //Console.WriteLine(arrayInt[i]);
+            }
+
+            foreach (int numeros in arrayInt)
+            {
+                Console.WriteLine(numeros);
+            }
+
+
+     
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
